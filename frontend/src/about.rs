5a77775ff825b907f9044f3fcba4d997a88689ad
myname1111 <animationstudiocp@gmail.com>
2022-10-10 19:08:55 +0700

    Add scafolding to about

--- frontend/src/about.rs
+++ frontend/src/about.rs
@@ -2,8 +2,78 @@ use yew::prelude::*;
 
 #[function_component(About)]
 pub fn about() -> Html {
-    let todo = String::from("Todo ").repeat(100);
     html! {
-        { todo }
+        <div>
+            <Sidebar>
+                <Bar header="Todo" />
+                <Bar header="Todo" />
+                <Bar header="Todo" />
+                <Bar header="Todo" />
+            </Sidebar>
+            <ContentList>
+                <Content>
+                    {"Todo"}
+                </Content>
+                <Content>
+                    {"Todo"}
+                </Content>
+                <Content>
+                    {"Todo"}
+                </Content>
+                <Content>
+                    {"Todo"}
+                </Content>
+            </ContentList>
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct SideBarProp {
+    children: ChildrenWithProps<Bar>,
+}
+
+#[function_component(Sidebar)]
+fn sidebar(props: &SideBarProp) -> Html {
+    html! {
+        { for props.children.iter() }
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct BarProp {
+    header: String,
+}
+
+#[function_component(Bar)]
+fn bar(_: &BarProp) -> Html {
+    html! {}
+}
+
+#[derive(Properties, PartialEq)]
+struct ContentListProp {
+    children: ChildrenWithProps<Content>,
+}
+
+#[function_component(ContentList)]
+fn content_list(props: &ContentListProp) -> Html {
+    html! {
+        <div>
+            { for props.children.iter() }
+        </div>
+    }
+}
+
+#[derive(Properties, PartialEq)]
+struct ContentProp {
+    children: Children,
+}
+
+#[function_component(Content)]
+fn content(props: &ContentProp) -> Html {
+    html! {
+        <div>
+            { for props.children.iter() }
+        </div>
     }
 }
