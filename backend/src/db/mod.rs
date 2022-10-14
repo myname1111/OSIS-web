4350ee3a9a8863c00b89d61cb8b02eb2a721d1c1
myname1111 <animationstudiocp@gmail.com>
2022-10-14 13:44:28 +0700

    Create db pool

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -1,15 +1,31 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::r2d2::ConnectionManager;
+use diesel::r2d2::Pool;
 use dotenvy::dotenv;
 use std::env;
 
 mod member;
 mod schema;
 
-pub fn connect() -> PgConnection {
+fn database_url() -> String {
     dotenv().ok();
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+}
+
+pub fn connect() -> PgConnection {
+    let database_url = database_url();
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn get_connection_pool() -> Pool<ConnectionManager<PgConnection>> {
+    let url = database_url();
+    let manager = ConnectionManager::<PgConnection>::new(url);
+
+    Pool::builder()
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Could not build connection pool")
 }
