e5c08b4ee9982373b13d6ad16ea240950b5b4ad5
myname1111 <animationstudiocp@gmail.com>
2022-10-14 12:53:30 +0700

    Prototype REST API

---
+++ backend/src/api/mod.rs
@@ -0,0 +1,14 @@
+/// Blog and blog posts
+mod blog;
+/// Division and worked on event / programs
+mod division;
+/// Events, reviews, improvements and schedule
+mod event;
+/// Forum post and forum plus visits
+mod forum;
+/// Images plus program and event images
+mod images;
+/// Member and president
+mod member;
+/// Program, reviews and improvemens
+mod program;
