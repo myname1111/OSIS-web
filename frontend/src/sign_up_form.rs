c038d982ae600c0cf0a44c57aa0f84b8981aeac0
myname1111 <animationstudiocp@gmail.com>
2022-11-20 16:45:07 +0700

    Update sign up form

--- frontend/src/sign_up_form.rs
+++ frontend/src/sign_up_form.rs
@@ -4,8 +4,7 @@ use web_sys::HtmlTextAreaElement;
 use yew::prelude::*;
 
 pub struct SignUpForm {
-    username: String,
-    password: String,
+    email: String,
 }
 
 impl Component for SignUpForm {
@@ -14,41 +13,26 @@ impl Component for SignUpForm {
 
     fn create(_: &Context<Self>) -> Self {
         Self {
-            username: "".to_string(),
-            password: "".to_string(),
+            email: "".to_string(),
         }
     }
 
     fn update(&mut self, _: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            SignUpFormMsg::SetUsername(x) => {
-                self.username = x;
-                true
-            }
-            SignUpFormMsg::SetPassword(x) => {
-                self.password = x;
+            SignUpFormMsg::SetEmail(x) => {
+                self.email = x;
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let set_field = |event: Event| {
+        let set_email = ctx.link().batch_callback(move |event: Event| {
             let target = event.target();
 
-            target.and_then(|t| t.dyn_into::<HtmlTextAreaElement>().ok())
-        };
+            let input = target.and_then(|t| t.dyn_into::<HtmlTextAreaElement>().ok());
 
-        let set_name = ctx.link().batch_callback(move |event: Event| {
-            let input = set_field(event);
-
-            input.map(|input| SignUpFormMsg::SetUsername(input.value()))
-        });
-
-        let set_reason = ctx.link().batch_callback(move |event: Event| {
-            let input = set_field(event);
-
-            input.map(|input| SignUpFormMsg::SetPassword(input.value()))
+            input.map(|input| SignUpFormMsg::SetEmail(input.value()))
         });
 
         html! {
@@ -57,16 +41,9 @@ impl Component for SignUpForm {
                 <form class="sign-up-form">
                     <div class="sign-up-form--form">
                         <div class="sign-up-form--info">
-                            <div class="sign-up-form--field">
-                                <label for="usern" class="sign-up-form--field-name">{ "Username" }</label>
-                                <textarea type="text" id="usern" name="usern" class="sign-up-form--field-input"
-                                    onchange={set_name}/>
-                            </div>
-                            <div class="sign-up-form--field">
-                                <label for="reason" class="sign-up-form--field-name">{ "Password" }</label>
-                                <textarea type="text" id="reason" name="reason" class="sign-up-form--field-input"
-                                    onchange={set_reason}/><div class="height-50px"/>
-                            </div>
+                            <label for="usern" class="sign-up-form--field-name">{ "Email" }</label>
+                            <textarea type="text" id="usern" name="usern" class="sign-up-form--field-input"
+                                onchange={set_email}/>
                         </div>
                         <div class="sign-up-form--sign-up-button-container">
                             <SignUpButton class="sign-up-button-today sign-up-button--hover"/>
@@ -79,7 +56,6 @@ impl Component for SignUpForm {
 }
 
 pub enum SignUpFormMsg {
-    SetUsername(String),
-    SetPassword(String),
+    SetEmail(String),
     // Submit; todo
 }
